7d2c32b534189ea048eefb592922e7b157ea1a05
datadiode <datadiode@users.noreply.github.com>
2021-03-07 06:50:25 +0100

    Remove 3rdParty/DateTimeParser.cs and exclude related tests, but leave a link to the article which published the original code

--- Toxy.Test/DateTimeParserTest.cs
+++ Toxy.Test/DateTimeParserTest.cs
@@ -1,11 +1,14 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Toxy.Test
 {
+    /// <summary>
+    /// Toxy makes no more use of DateTimeParser.Parse(), hence tests are excluded.
+    /// The removed implementation of DateTimeParser.Parse() was reusing code from
+    /// https://hashfactor.wordpress.com/2009/02/02/c-parsing-datetime-with-timezone/
+    /// </summary>
+#if false
     [TestFixture]
     public class DateTimeParserTest
     {
@@ -15,7 +18,6 @@ namespace Toxy.Test
             Assert.AreEqual(DateTime.Parse("2008-10-25 03:09:06 +8"), DateTimeParser.Parse("24-oct-08 21:09:06 CEST"));
             Assert.AreEqual(DateTime.Parse("2012-04-20 16:10:00 +8"), DateTimeParser.Parse("2012 -04-20 10:10:00+0200"));
             Assert.AreEqual(DateTime.Parse("2014-12-12 12:13:30 +8"), DateTimeParser.Parse("Fri, 12 Dec 2014 12:13:30 +0800 (CST)"));
-
         }
 
         [Test]
@@ -24,4 +26,5 @@ namespace Toxy.Test
             Assert.AreEqual(new DateTime(2014, 12, 12, 12, 13, 30), DateTimeParser.Parse("Fri, 12 Dec 2014 12:13:30"));
         }
     }
+#endif
 }
